d36adff5a88217802ae5ac0f65b5b839f1fad8e0
Nishaykumar <Nishaykumar24@gmail.com>
2023-07-13 16:02:56 +0530

    Initial commit

---
+++ TriviaGameDemo/Components/PrimaryButton.swift
@@ -0,0 +1,28 @@
+//
+//  PrimaryButton.swift
+//  TriviaGameDemo
+//
+//  Created by Nishay Kumar on 13/07/23.
+//
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    let text: String
+    let background: Color = Color.accentColor
+    var body: some View {
+        Text(text)
+            .foregroundColor(.white)
+            .padding()
+            .padding(.horizontal)
+            .background(background)
+            .cornerRadius(30)
+            .shadow(radius: 10)
+    }
+}
+
+struct PrimaryButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PrimaryButton(text: "Next")
+    }
+}
